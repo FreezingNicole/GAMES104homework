5f9f162d6e4c1a0dd586628c28b2f35fcfd8db7a
PlusV <271433236@qq.com>
2022-03-27 16:37:49 +0800

    第一次提交

--- Assets/New Folder/test.cs
+++ Assets/New Folder/test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    private string str = "first";
+    private string str = "firs";
     // Start is called before the first frame update
     void Start()
     {
